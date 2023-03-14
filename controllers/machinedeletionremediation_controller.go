c6695c88ffe401fa01f87ba166927b9cac988e31
Carlo Lobrano <c.lobrano@gmail.com>
2023-03-14 10:21:43 +0000

    Remove unnecessary module alias

--- controllers/machinedeletionremediation_controller.go
+++ controllers/machinedeletionremediation_controller.go
@@ -23,7 +23,7 @@ import (
 
 	"github.com/go-logr/logr"
 
-	v1 "k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"

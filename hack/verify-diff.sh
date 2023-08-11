1c2197820fc6c474a1007fa0f5091cd50213fe5d
Carlo Lobrano <c.lobrano@gmail.com>
2023-08-11 10:51:54 +0200

    Refine test workflows and integrate uncommitted changes check
    - Update test target in Makefile to run unit tests followed by
      uncommitted changes verification.
    - Enhance `verify-diff.sh` to detect uncommitted changes and
      out-of-date bundle.
    - Remove mutation testing references from CI and Makefile.
    
    Signed-off-by: Carlo Lobrano <c.lobrano@gmail.com>

--- hack/verify-diff.sh
+++ hack/verify-diff.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-FILE_DIFF=$(git ls-files -o --exclude-standard)
 
-if [ "$FILE_DIFF" != "" ]; then
-  echo "Found untracked files:"
-  echo "$FILE_DIFF"
-  exit 1
+status=0
+# Checks that there are no uncommitted changes.
+if [[ -n "$(git status --porcelain .)" ]]; then
+    echo "[!] Uncommitted files. Commit changes and run 'make test'."
+    # Ignore untracked files to avoid messing up with the CI.
+    echo "$(git status --porcelain .)"
+    let status=1
 fi
 
-git diff --exit-code
+# Checks that changes under config directory are reflected in the bundle directory.
+# This is to ensure that the bundle is up-to-date with the latest changes.
+# If this fails, run 'make bundle' to update the bundle.
+if [[ -n "$(git status --porcelain config/)" ]] && [[ -z "$(git status --porcelain bundle/)"  ]]; then
+    echo ""
+    echo "[!] Config folder has changes, but Bundle does not. Check if Bundle is out-of-date."
+    echo "$(git status --porcelain config/)"
+    let status=1
+fi
+
+exit $status
